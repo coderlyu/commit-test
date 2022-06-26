2ccc0d8bf55709aa22576f700a51644c21572237
刘誉 <liuyu02@weidian.com>
2022-06-26 21:37:57 +0800

    feat: 26

---
+++ questions/array/26.js
@@ -0,0 +1,37 @@
+/**
+ * level: 简单
+ * 
+ * 删除有序数组中的重复项
+ * https://leetcode.cn/problems/remove-duplicates-from-sorted-array/
+ * 
+ * 给你一个 升序排列 的数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。元素的 相对顺序 应该保持 一致 。
+
+由于在某些语言中不能改变数组的长度，所以必须将结果放在数组nums的第一部分。更规范地说，如果在删除重复项之后有 k 个元素，那么 nums 的前 k 个元素应该保存最终结果。
+
+将最终结果插入 nums 的前 k 个位置后返回 k 。
+
+不要使用额外的空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+
+*/
+
+
+// 输入：nums = [0,0,1,1,1,2,2,3,3,4]
+// 输出：5, nums = [0,1,2,3,4]
+// 解释：函数应该返回新的长度 5 ， 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4 。不需要考虑数组中超出新长度后面的元素。
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    if (nums.length < 2) return nums
+    let L = 0, R = L + 1
+    while(R < nums.length) {
+        if (nums[L] === nums[R]) R++
+        else nums[++L] = nums[R]
+    }
+    return L + 1
+};
+
+const nums = [0,0,1,1,1,2,2,3,3,4]
+console.log(removeDuplicates(nums))
