ff4769872a568c481009f5b9c1c44684b1b854a7
coderly <974257574@qq.com>
2022-07-06 09:29:22 +0800

    feat: 12

---
+++ questions/string/12.js
@@ -0,0 +1,69 @@
+/**
+ * level: 中等
+ * 
+ * 整数转罗马数字
+ * https://leetcode.cn/problems/integer-to-roman/
+ * 
+ * 罗马数字包含以下七种字符： I， V， X， L，C，D 和 M。
+
+字符          数值
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+例如， 罗马数字 2 写做 II ，即为两个并列的 1。12 写做 XII ，即为 X + II 。 27 写做  XXVII, 即为 XX + V + II 。
+
+通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况：
+
+I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
+X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。 
+C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
+给你一个整数，将其转为罗马数字。
+ */
+
+// 输入: num = 9
+// 输出: "IX"
+
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function (num) {
+  if(num < 1) return ''
+  const valueToRomanmap = {
+    1: 'I',
+    4: 'IV',
+    5: 'V',
+    9: 'IX',
+    10: 'X',
+    40: 'XL',
+    50: 'L',
+    90: 'XC',
+    100: 'C',
+    400: 'CD',
+    500: 'D',
+    900: 'CM',
+    1000: 'M',
+  };
+  const value = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+
+  let str = "";
+  let i = 0
+  while (num > 0) {
+    i = 0
+    while(num - value[i] < 0 && i < value.length) {
+      i++
+    }
+    str+=valueToRomanmap[value[i]]
+    num = num - value[i]
+  }
+  return str
+};
+
+// 输入: num = 58
+// 输出: "LVIII"
+const num = 3; // 9 --> IX; 58 --> LVIII; 1994 --> MCMXCIV
+console.log(intToRoman(num));
